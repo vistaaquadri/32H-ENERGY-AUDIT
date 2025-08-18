ae97bf2156504e6ebe9a52a45b7d1ea754cb54c4
AbdulFatai <aquadri@vista-advisory.com>
2025-08-18 11:01:28 +0100

    Reporting dashboard

---
+++ Reporting Dashboard/dashboard/www/scripts/code.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $(".navbar .container-fluid").append(
+    '<a href="https://www.rea.gov.ng" target="_blank">' +
+      '<img src="images/rea-logo.png" align="right" style="height:42px;">' +
+    '</a>'
+  );
+});
